08e7d8f057bbbc76d27fe18d5db88b206d7168c7
xhayper <hayper1919@gmail.com>
2022-11-03 17:14:35 +0700

    feat: make the save button work

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,28 @@
+<script setup lang="ts">
+import { useSaveData } from "~/stores/saveData";
+
+const saveData = useSaveData();
+
+const downloadSaveFile = async (e: MouseEvent) => {
+    e.preventDefault();
+    if (!document) return;
+    const blob = new Blob([await saveData.exportSave(saveData.fileData!.encrypted)], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const anchor = document.createElement("a");
+    anchor.href = url;
+    anchor.download = saveData.fileData!.name;
+    anchor.click();
+    URL.revokeObjectURL(url);
+};
+</script>
+
 <template>
     <footer class="footer fixed-bottom bg-light py-2">
         <p class="alignleft">
             <span class="text-muted text-center">Made with ❤ by SavagE</span>
         </p>
         <p class="aligncenter">
-            <!-- TODO: make this actualyl work -->
-            <input type="submit" id="formSubmitBtn" class="btn btn-lg btn-primary" form="form" value="Save">
+            <input type="submit" class="btn btn-lg btn-primary" form="form" value="Save" @click="downloadSaveFile">
         </p>
     </footer>
 </template>
