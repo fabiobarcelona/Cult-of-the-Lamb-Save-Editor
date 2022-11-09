9a3af66eac505aa374d5545d6e580b70b06e7aff
xhayper <hayper1919@gmail.com>
2022-11-09 11:36:39 +0700

    feat: useHead

--- app.vue
+++ app.vue
@@ -10,4 +10,15 @@
 
 <script setup lang="ts">
 import "source-map-support/register";
+
+useHead({
+  title: "Cult of the Lamb - Save editor",
+  htmlAttrs: {
+    "lang": "en"
+  },
+  meta: [
+    { name: "color-scheme", content: "dark" },
+    { name: "theme-color", content: "#eeeeee" }
+  ]
+})
 </script>
