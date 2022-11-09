0361b7592add4b5378bae8ecb322a53bdf7e7405
xhayper <hayper1919@gmail.com>
2022-11-09 13:33:49 +0700

    feat: use better font

--- app.vue
+++ app.vue
@@ -16,9 +16,13 @@ useHead({
   htmlAttrs: {
     "lang": "en"
   },
+  link: [
+    { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+    { rel: "stylesheet", href: "https://rsms.me/inter/inter.css" }
+  ],
   meta: [
     { name: "color-scheme", content: "dark" },
-    { name: "theme-color", content: "#eeeeee" }
+    { name: "theme-color", content: "#eeeeee" },
   ]
 })
 </script>
